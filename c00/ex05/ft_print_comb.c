b526e4f6f03b2af51f23a8244d2600eb14de3726
Carlos Jr <jrguedelha@gmail.com>
2021-06-20 16:58:19 -0300

    ex05 adicionado

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void	ft_putchars(char c1, char c2, char c3)
+{
+	write(1, &c1, 1);
+	write(1, &c2, 1);
+	write(1, &c3, 1);
+	if (c1 != '7')
+		write(1, ", ", 2);
+}
+
+void	ft_print_comb(void)
+{
+	char	c1;
+	char	c2;
+	char	c3;
+
+	c1 = '0';
+	while (c1 <= '7')
+	{
+		c2 = c1 + 1;
+		while (c2 <= '8')
+		{
+			c3 = c2 + 1;
+			while (c3 <= '9')
+			{
+				ft_putchars(c1, c2, c3);
+				++c3;
+			}
+			++c2;
+		}
+		++c1;
+	}
+}
